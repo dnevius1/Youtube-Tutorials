71576574af5804257b14b61726ed1178f37de619
Daniel Nevius <71950440+dnevius1@users.noreply.github.com>
2023-10-23 13:03:37 -0500

    Add files via upload

---
+++ react-http-gh/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { Component } from 'react'
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+
+function App() {
+  return (
+    <div className="App">
+     <PostForm />
+    </div>
+  )
+}
+
+export default App;
